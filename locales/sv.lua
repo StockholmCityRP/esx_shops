31dc22f11f64409fc34dff90894d630e5b55f166
Kasper <35449118+KasperE1@users.noreply.github.com>
2018-02-12 13:26:59 +0100

    Create sv.lua

---
+++ locales/sv.lua
@@ -0,0 +1,9 @@
+Locales['sv'] = {
+
+	['shop'] = 'Affär',
+	['shops'] = 'Affärer',
+	['press_menu'] = 'Tryck ~INPUT_CONTEXT~ för att handla.',
+	['bought'] = 'Du köpte ~b~1x ',
+	['not_enough'] = 'Du har inte ~r~tillräckligt~s~ med pengar.'
+
+}
